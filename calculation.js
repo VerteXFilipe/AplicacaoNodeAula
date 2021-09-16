2200913ae550b25a1f03b3f3b91abe60eaf07604
VerteXFilipe <filipeavila2000@hotmail.com>
2021-09-15 22:05:00 -0300

    comitei

---
+++ calculation.js
@@ -0,0 +1,22 @@
+function soma(num1,num2) {
+    return num1+num2;
+}
+
+function subtracao(num1,num2) {
+    return num1-num2;
+}
+
+function multiplicacao(num1,num2) {
+    return num1*num2;
+}
+
+function divisao(num1,num2) {
+    return num1/num2;
+}
+
+module.exports = {
+ soma,
+ multiplicacao,
+ divisao,
+ subtracao
+}
